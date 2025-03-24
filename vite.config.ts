40d599e0e36f0d97aa58e458e4b2a4816e64efa0
Leonel Baltazar Bravo <96897286+pybalt@users.noreply.github.com>
2025-03-23 23:16:50 -0300

    Add Terser minification options in Vite configuration
    - Configured the build process to use Terser for minification.
    - Added options to drop console logs and debuggers during the build, enhancing code cleanliness and performance.
    
    These changes improve the build output by reducing unnecessary code in production.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   envDir: '.',
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 })
